842e0aff2c5fc92142bb42925694f16ef42f8b54
Francis Tin-ao <franfra10j@gmail.com>
2022-07-12 11:29:27 +0800

    update

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const hamburger = document.querySelector('.hamburger');
+const cursor = document.querySelector('.cursor');
 const mobile_menu = document.querySelector('.mobile-nav');
 var mobile_anchor = document.getElementById('mobile-a');
 var icon = document.getElementById('ld-mode');
 var mobile_icon = document.getElementById('mobileld-mode');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style","top: "+(e.pageY - 20)+"px; left: "+(e.pageX - 20)+"px;")
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
 
 hamburger.addEventListener('click', function () {
     this.classList.toggle('is-active');
